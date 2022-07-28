d8d6fc56422b8f6ea5b0635b66ac83671b0d0e3b
Yuki Tomita <tomtoma1025@gmail.com>
2022-07-28 18:21:33 +0900

    fix test

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
-        <a>About test</a>
+        <a>About test2</a>
       </Link>
     </p>
   </Layout>
